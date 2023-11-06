6cee921941825645fc6e060f270efb651b1bfcb6
frostedoyster <bigi.f@libero.it>
2023-11-06 20:06:23 +0100

    Function signatures

---
+++ python/mops/src/mops/reference_implementations.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def hpe(C, A, P):
+    pass
+
+
+def sap(C, A, B, P_A, P_B, P_O, n_O):
+    pass
+
+
+def opsa(A, B, P):
+    pass
+
+
+def opsax(A, R, X, I, J):
+    pass
+
+
+def sasax(C, A, R, X, I, J, M_A, M_X, M_O, n_O):
+    pass
