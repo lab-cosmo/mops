38f26caa977994af08af224ec6d9c2a382891d66
frostedoyster <bigi.f@libero.it>
2023-11-10 20:06:38 +0100

    Format after merge

--- mops/include/mops.h
+++ mops/include/mops.h
@@ -3,11 +3,11 @@
 
 #include "mops/exports.h" // IWYU pragma: export
 
-#include "mops/hpe.h"     // IWYU pragma: export
-#include "mops/opsa.h"    // IWYU pragma: export
-#include "mops/opsax.h"   // IWYU pragma: export
-#include "mops/sap.h"     // IWYU pragma: export
-#include "mops/sasax.h"   // IWYU pragma: export
+#include "mops/hpe.h"   // IWYU pragma: export
+#include "mops/opsa.h"  // IWYU pragma: export
+#include "mops/opsax.h" // IWYU pragma: export
+#include "mops/sap.h"   // IWYU pragma: export
+#include "mops/sasax.h" // IWYU pragma: export
 
 #ifdef __cplusplus
 extern "C" {
