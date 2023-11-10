c633259998ae6105035251fbb17fe482f29e12ed
frostedoyster <bigi.f@libero.it>
2023-11-10 09:18:58 +0100

    One more `opsa` test

--- python/mops/tests/opsa.py
+++ python/mops/tests/opsa.py
@@ -7,7 +7,19 @@ from mops.reference_implementations import outer_product_scatter_add as ref_opsa
 from mops import outer_product_scatter_add as opsa
 
 
-def test_opsa_numpy():
+def test_opsa():
+
+    A = np.random.rand(100, 20)
+    B = np.random.rand(100, 5)
+
+    indices = np.sort(np.random.randint(10, size=(100,)))
+
+    reference = ref_opsa(A, B, indices, np.max(indices)+1)
+    actual = opsa(A, B, indices, np.max(indices)+1)
+    assert np.allclose(reference, actual)
+
+
+def test_opsa_no_neighbors():
 
     A = np.random.rand(100, 20)
     B = np.random.rand(100, 5)
