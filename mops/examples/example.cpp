1225a2ffea08bc0147a566324df170b158f429ff
frostedoyster <bigi.f@libero.it>
2023-11-11 20:56:57 +0100

    Format

--- mops/examples/example.cpp
+++ mops/examples/example.cpp
@@ -1,20 +1,16 @@
-#include <vector>
 #include "mops.hpp"
-
+#include <vector>
 
 int main() {
 
-    auto A = std::vector<double>(100*100);
+    auto A = std::vector<double>(100 * 100);
     auto C = std::vector<double>(100);
-    auto P = std::vector<int>(100*4);
+    auto P = std::vector<int>(100 * 4);
     auto O = std::vector<double>(100);
 
     mops::homogeneous_polynomial_evaluation<double>(
-        {O.data(), {100}},
-        {A.data(), {100, 100}},
-        {C.data(), {100}},
-        {P.data(), {100, 4}}
-    );
+        {O.data(), {100}}, {A.data(), {100, 100}}, {C.data(), {100}},
+        {P.data(), {100, 4}});
 
     return 0;
 }
