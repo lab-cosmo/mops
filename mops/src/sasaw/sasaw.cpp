c4e3ccc23bbdf5e8adfc3f2bc64a189b33f681b4
frostedoyster <bigi.f@libero.it>
2023-11-23 11:58:29 +0100

    Fix final bugs

--- mops/src/sasaw/sasaw.cpp
+++ mops/src/sasaw/sasaw.cpp
@@ -18,10 +18,9 @@ template void mops::sparse_accumulation_scatter_add_with_weights<double>(
 #else
 template <typename scalar_t>
 void mops::cuda::sparse_accumulation_scatter_add_with_weights(
-    Tensor<scalar_t, 3> output, Tensor<scalar_t, 2> A, Tensor<scalar_t, 2> B,
-    Tensor<scalar_t, 1> C, Tensor<scalar_t, 3> W, Tensor<int, 1> indices_A,
-    Tensor<int, 1> indices_W_1, Tensor<int, 1> indices_W_2,
-    Tensor<int, 1> indices_output_1, Tensor<int, 1> indices_output_2) {
+    Tensor<scalar_t, 3>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>,
+    Tensor<scalar_t, 1>, Tensor<scalar_t, 3>, Tensor<int, 1>, Tensor<int, 1>,
+    Tensor<int, 1>, Tensor<int, 1>, Tensor<int, 1>) {
     throw std::runtime_error("MOPS was not compiled with CUDA support");
 }
 
