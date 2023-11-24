780c4120e6cffa08f6dd626b542b3e73e68dd700
frostedoyster <bigi.f@libero.it>
2023-11-24 08:36:21 +0100

    Torch tests and format

--- mops-torch/include/mops/torch.hpp
+++ mops-torch/include/mops/torch.hpp
@@ -2,9 +2,9 @@
 #define MOPS_TORCH_H
 
 #include "torch/hpe.hpp"   // IWYU pragma: export
-#include "torch/opsa.hpp"   // IWYU pragma: export
+#include "torch/opsa.hpp"  // IWYU pragma: export
+#include "torch/opsaw.hpp" // IWYU pragma: export
 #include "torch/sap.hpp"   // IWYU pragma: export
-#include "torch/opsaw.hpp"   // IWYU pragma: export
-#include "torch/sasaw.hpp"   // IWYU pragma: export
+#include "torch/sasaw.hpp" // IWYU pragma: export
 
 #endif
