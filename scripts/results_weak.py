d5539a53705e67708e3d88b4fc981513fcdbd055
frostedoyster <bigi.f@libero.it>
2023-11-27 18:14:43 +0100

    What??

--- scripts/results_weak.py
+++ scripts/results_weak.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read the data from the file
+with open("weak_omp.out", 'r') as file:
+    lines = file.readlines()
+filtered_lines = [line for line in lines if 'Average' in line]
+results = [float(line.split()[-2]) for line in filtered_lines]
+results_per_category = np.array(results).reshape(5, 6, 2)
+results_per_category_omp = results_per_category.swapaxes(1, 2).reshape(10, 6)
+
 with open("weak_tbb.out", 'r') as file:
     lines = file.readlines()
-
-# Filter out lines that contain 'Standard Deviation'
 filtered_lines = [line for line in lines if 'Average' in line]
-
 results = [float(line.split()[-2]) for line in filtered_lines]
-
 results_per_category = np.array(results).reshape(5, 6, 2)
-
-results_per_category = results_per_category.swapaxes(1, 2).reshape(10, 6)
+results_per_category_tbb = results_per_category.swapaxes(1, 2).reshape(10, 6)
 
 x_axis = [1, 2, 4, 8, 16, 32]
 for i_operation, operation_name in enumerate(["hpe", "hpe_vjp", "opsa", "opsa_vjp", "sap", "sap_vjp", "opsaw", "opsaw_vjp", "sasaw", "sasaw_vjp"]):
     if i_operation % 2 == 0: plt.figure()
     plt.plot(x_axis, [1, 1, 1, 1, 1, 1], color="black")
-    plt.plot(x_axis, results_per_category[i_operation][0]/results_per_category[i_operation], label=operation_name)
+    plt.plot(x_axis, results_per_category_omp[i_operation][0]/results_per_category_omp[i_operation], label=operation_name + " (OpenMP)")
+    plt.plot(x_axis, results_per_category_omp[i_operation][0]/results_per_category_tbb[i_operation], label=operation_name + " (TBB)")
     plt.x_lim = (1, 32)
     plt.y_lim = (1, 32)
     plt.xscale('log')
