daf7b055eb001c8940dc9531b2602d71b1172776
frostedoyster <bigi.f@libero.it>
2023-12-03 00:51:07 +0100

    Write lint commands

---
+++ scripts/check-format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for file in $(find . -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.cu" -o -name "*.cuh" \)  \
+-not -path "*/external/*" -not -path "*/build/*" -not -path "*/.tox/*"); do
+    clang-format -i "$file"
+    if git diff --quiet "$file"; then
+    echo "✅ $file is properly formatted."
+    else
+    echo "❌ $file is not properly formatted. Please run './scripts/format.sh' from the mops root directory"
+    exit 1
+    fi
+done
