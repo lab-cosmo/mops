b08beeb7b204d4b39d6150086dd4910ed32e1dd3
frostedoyster <bigi.f@libero.it>
2024-02-15 17:03:31 +0100

    Improve `opsa`

--- mops/src/opsa/capi.cpp
+++ mops/src/opsa/capi.cpp
@@ -15,7 +15,7 @@ static size_t checked_cast(int64_t value) {
 }
 
 extern "C" int mops_outer_product_scatter_add_f32(
-    mops_tensor_2d_f32_t output,
+    mops_tensor_3d_f32_t output,
     mops_tensor_2d_f32_t A,
     mops_tensor_2d_f32_t B,
     mops_tensor_1d_i32_t indices_output
@@ -23,7 +23,9 @@ extern "C" int mops_outer_product_scatter_add_f32(
 
     MOPS_CATCH_EXCEPTIONS_BEGIN
         mops::outer_product_scatter_add<float>(
-            {output.data, {checked_cast(output.shape[0]), checked_cast(output.shape[1])}},
+            {output.data, {checked_cast(output.shape[0]),
+                                checked_cast(output.shape[1]),
+                                checked_cast(output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
@@ -32,14 +34,16 @@ extern "C" int mops_outer_product_scatter_add_f32(
 }
 
 extern "C" int mops_outer_product_scatter_add_f64(
-    mops_tensor_2d_f64_t output,
+    mops_tensor_3d_f64_t output,
     mops_tensor_2d_f64_t A,
     mops_tensor_2d_f64_t B,
     mops_tensor_1d_i32_t indices_output
 ) {
     MOPS_CATCH_EXCEPTIONS_BEGIN
         mops::outer_product_scatter_add<double>(
-            {output.data, {checked_cast(output.shape[0]), checked_cast(output.shape[1])}},
+            {output.data, {checked_cast(output.shape[0]),
+                                checked_cast(output.shape[1]),
+                                checked_cast(output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
@@ -50,7 +54,7 @@ extern "C" int mops_outer_product_scatter_add_f64(
 extern "C" int mops_outer_product_scatter_add_vjp_f32(
     mops_tensor_2d_f32_t grad_A,
     mops_tensor_2d_f32_t grad_B,
-    mops_tensor_2d_f32_t grad_output,
+    mops_tensor_3d_f32_t grad_output,
     mops_tensor_2d_f32_t A,
     mops_tensor_2d_f32_t B,
     mops_tensor_1d_i32_t indices_output
@@ -61,7 +65,9 @@ extern "C" int mops_outer_product_scatter_add_vjp_f32(
             {grad_A.data, {checked_cast(grad_A.shape[0]), checked_cast(grad_A.shape[1])}},
             {grad_B.data, {checked_cast(grad_B.shape[0]), checked_cast(grad_B.shape[1])}},
             {grad_output.data,
-             {checked_cast(grad_output.shape[0]), checked_cast(grad_output.shape[1])}},
+             {checked_cast(grad_output.shape[0]),
+              checked_cast(grad_output.shape[1]),
+              checked_cast(grad_output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
@@ -72,7 +78,7 @@ extern "C" int mops_outer_product_scatter_add_vjp_f32(
 extern "C" int mops_outer_product_scatter_add_vjp_f64(
     mops_tensor_2d_f64_t grad_A,
     mops_tensor_2d_f64_t grad_B,
-    mops_tensor_2d_f64_t grad_output,
+    mops_tensor_3d_f64_t grad_output,
     mops_tensor_2d_f64_t A,
     mops_tensor_2d_f64_t B,
     mops_tensor_1d_i32_t indices_output
@@ -82,7 +88,9 @@ extern "C" int mops_outer_product_scatter_add_vjp_f64(
             {grad_A.data, {checked_cast(grad_A.shape[0]), checked_cast(grad_A.shape[1])}},
             {grad_B.data, {checked_cast(grad_B.shape[0]), checked_cast(grad_B.shape[1])}},
             {grad_output.data,
-             {checked_cast(grad_output.shape[0]), checked_cast(grad_output.shape[1])}},
+             {checked_cast(grad_output.shape[0]),
+              checked_cast(grad_output.shape[1]),
+              checked_cast(grad_output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
@@ -91,14 +99,16 @@ extern "C" int mops_outer_product_scatter_add_vjp_f64(
 }
 
 extern "C" int mops_cuda_outer_product_scatter_add_f32(
-    mops_tensor_2d_f32_t output,
+    mops_tensor_3d_f32_t output,
     mops_tensor_2d_f32_t A,
     mops_tensor_2d_f32_t B,
     mops_tensor_1d_i32_t indices_output
 ) {
     MOPS_CATCH_EXCEPTIONS_BEGIN
         mops::cuda::outer_product_scatter_add<float>(
-            {output.data, {checked_cast(output.shape[0]), checked_cast(output.shape[1])}},
+            {output.data, {checked_cast(output.shape[0]),
+                                checked_cast(output.shape[1]),
+                                checked_cast(output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
@@ -107,14 +117,16 @@ extern "C" int mops_cuda_outer_product_scatter_add_f32(
 }
 
 extern "C" int mops_cuda_outer_product_scatter_add_f64(
-    mops_tensor_2d_f64_t output,
+    mops_tensor_3d_f64_t output,
     mops_tensor_2d_f64_t A,
     mops_tensor_2d_f64_t B,
     mops_tensor_1d_i32_t indices_output
 ) {
     MOPS_CATCH_EXCEPTIONS_BEGIN
         mops::cuda::outer_product_scatter_add<double>(
-            {output.data, {checked_cast(output.shape[0]), checked_cast(output.shape[1])}},
+            {output.data, {checked_cast(output.shape[0]),
+                                checked_cast(output.shape[1]),
+                                checked_cast(output.shape[2])}},
             {A.data, {checked_cast(A.shape[0]), checked_cast(A.shape[1])}},
             {B.data, {checked_cast(B.shape[0]), checked_cast(B.shape[1])}},
             {indices_output.data, {checked_cast(indices_output.shape[0])}}
---
+++ python/mops-torch/benchmarks/opsa.py
@@ -0,0 +1,19 @@
+import mops.torch
+import torch
+from benchmark import benchmark, format_mean_std
+
+torch.manual_seed(0xDEADBEEF)
+
+
+A = torch.rand(1000, 20, requires_grad=True)
+B = torch.rand(1000, 5, requires_grad=True)
+output_size = 1000
+indices = torch.randint(10, size=(1000,), dtype=torch.int32)
+
+
+mean_fwd, std_fwd, mean_bwd, std_bwd = benchmark(
+    lambda: torch.sum(mops.torch.outer_product_scatter_add(A, B, indices, output_size))
+)
+
+print("Forward pass:", format_mean_std(mean_fwd, std_fwd))
+print("Backward pass:", format_mean_std(mean_bwd, std_bwd))
