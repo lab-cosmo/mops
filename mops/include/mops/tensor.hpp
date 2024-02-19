9da20d41d52fe339a54529054e9c1259f5ec278f
Luthaf <luthaf@luthaf.fr>
2024-02-19 16:12:03 +0100

    Add a check for restrict syntax in cmake

--- mops/include/mops/tensor.hpp
+++ mops/include/mops/tensor.hpp
@@ -7,7 +7,7 @@ namespace mops {
 template <typename scalar_t, size_t N_DIMS> struct Tensor {
     /// Pointer to the first element of the tensor. The data must be
     /// contiguous and in row-major order.
-    scalar_t *__restrict__ data;
+    scalar_t *MOPS_RESTRICT data;
     /// Shape of the tensor
     size_t shape[N_DIMS];
 };
