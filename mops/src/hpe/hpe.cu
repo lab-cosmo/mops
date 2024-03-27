35a540c37685ccab300f3933533bf850bf86020f
Nick J. Browning <nickjbrowning@gmail.com>
2024-03-27 11:21:27 +0100

    inital HPE VJP implementation.

--- mops/src/hpe/hpe.cu
+++ mops/src/hpe/hpe.cu
@@ -1,10 +1,7 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <vector>
-
-#include "mops/checks.hpp"
-#include "mops/cuda_utils.cuh"
 #include "mops/hpe.hpp"
+
+#include "internal/checks.hpp"
+#include "internal/cuda_utils.cuh"
 
 using namespace mops;
 using namespace mops::cuda;
@@ -175,4 +172,170 @@ template void mops::cuda::homogeneous_polynomial_evaluation<float>(
 
 template void mops::cuda::homogeneous_polynomial_evaluation<double>(
     Tensor<double, 1> output, Tensor<double, 2> A, Tensor<double, 1> C, Tensor<int32_t, 2> indices_A
+);
+
+template <typename scalar_t, int32_t polynomial_order>
+__global__ void homogeneous_polynomial_evaluation_vjp_kernel(
+    Tensor<scalar_t, 2> grad_A,
+    Tensor<scalar_t, 1> grad_output,
+    Tensor<scalar_t, 2> A,
+    Tensor<scalar_t, 1> C,
+    Tensor<int32_t, 2> indices_A
+
+) {
+    extern __shared__ char buffer[];
+
+    int32_t nbatch = A.shape[0];
+    int32_t nnu1 = A.shape[1];
+    int32_t nbasis = C.shape[0];
+
+    void* sptr = buffer;
+    size_t space = 0;
+
+    /* shared buffers */
+    scalar_t* buffer_nu1 = shared_array<scalar_t>(nnu1, sptr, &space);
+    scalar_t* buffer_gradA = shared_array<scalar_t>(nnu1, sptr, &space);
+
+    int32_t batch_id = blockIdx.x;
+
+    if (batch_id > nbatch) {
+        return;
+    }
+
+    // load all of A into shared memory
+    for (int32_t i = threadIdx.x; i < nnu1; i += blockDim.x) {
+        buffer_nu1[i] = A.data[batch_id * nnu1 + i];
+        buffer_gradA[i] = 0.0;
+    }
+
+    __syncthreads();
+
+    scalar_t gout = grad_output.data[batch_id];
+
+    for (int32_t basis = threadIdx.x; basis < nbasis; basis += blockDim.x) {
+
+        scalar_t c = C.data[basis];
+
+        for (int32_t i_monomial = 0; i_monomial < polynomial_order; i_monomial++) {
+
+            scalar_t tmp_i = c * gout;
+
+            for (int32_t j_monomial = 0; j_monomial < polynomial_order; j_monomial++) {
+
+                if (i_monomial == j_monomial) {
+                    continue;
+                }
+
+                int32_t idx_j = indices_A.data[j_monomial * indices_A.shape[0] + basis];
+
+                tmp_i *= buffer_nu1[idx_j];
+            }
+
+            int idx_i = indices_A.data[i_monomial * indices_A.shape[0] + basis];
+
+            atomicAdd(&buffer_gradA[idx_i], tmp_i);
+        }
+    }
+
+    __syncthreads();
+
+    for (int32_t i = threadIdx.x; i < nnu1; i += blockDim.x) {
+        grad_A.data[batch_id * nnu1 + i] = buffer_gradA[i];
+    }
+}
+
+template <typename scalar_t>
+void mops::cuda::homogeneous_polynomial_evaluation_vjp(
+    Tensor<scalar_t, 2> grad_A,
+    Tensor<scalar_t, 1> grad_output,
+    Tensor<scalar_t, 2> A,
+    Tensor<scalar_t, 1> C,
+    Tensor<int32_t, 2> indices_A
+) {
+
+    int nbatch = grad_output.shape[0];
+    int nnu1 = A.shape[1];
+
+    dim3 block_dim(nbatch);
+
+    dim3 thread_block(128, 1, 1);
+
+    void* sptr = nullptr;
+    size_t space = 0;
+
+    shared_array<scalar_t>(2 * nnu1, sptr, &space);
+
+    size_t polynomial_order = indices_A.shape[1];
+
+    if (polynomial_order <= 10) {
+        switch (polynomial_order) {
+        case 0:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 0>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 1:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 1>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 2:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 2>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 3:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 3>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 4:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 4>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 5:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 5>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 6:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 6>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 7:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 7>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 8:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 8>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 9:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 9>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        case 10:
+            homogeneous_polynomial_evaluation_vjp_kernel<scalar_t, 10>
+                <<<block_dim, thread_block, space>>>(grad_A, grad_output, A, C, indices_A);
+            break;
+        default:
+            break;
+        }
+    }
+
+    CUDA_CHECK_ERROR(cudaGetLastError());
+
+    CUDA_CHECK_ERROR(cudaDeviceSynchronize());
+}
+
+// explicit instanciations of CUDA templates
+template void mops::cuda::homogeneous_polynomial_evaluation_vjp<float>(
+    Tensor<float, 2> gradA,
+    Tensor<float, 1> grad_output,
+    Tensor<float, 2> A,
+    Tensor<float, 1> C,
+    Tensor<int32_t, 2> indices_A
+);
+
+template void mops::cuda::homogeneous_polynomial_evaluation_vjp<double>(
+    Tensor<double, 2> gradA,
+    Tensor<double, 1> grad_output,
+    Tensor<double, 2> A,
+    Tensor<double, 1> C,
+    Tensor<int32_t, 2> indices_A
 );
