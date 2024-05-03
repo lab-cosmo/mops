1c7424b765847d0beba6731dd093e88f80b540fd
Nick J. Browning <nickjbrowning@gmail.com>
2024-05-03 14:01:48 +0200

    fixed issue with opsaw instantiation

--- mops/src/opsaw/opsaw.cpp
+++ mops/src/opsaw/opsaw.cpp
@@ -78,32 +78,19 @@ template void mops::outer_product_scatter_add_with_weights_vjp_vjp<double>(
 #ifndef MOPS_CUDA_ENABLED
 template <typename scalar_t>
 void mops::cuda::
-    outer_product_scatter_add_with_weights(Tensor<scalar_t, 3>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<int32_t, 1>, Tensor<int32_t, 1>) {
+    outer_product_scatter_add_with_weights(Tensor<scalar_t, 3>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<int32_t, 1>, Tensor<int32_t, 1>, void*) {
     throw std::runtime_error("MOPS was not compiled with CUDA support");
 }
 
 template <typename scalar_t>
 void mops::cuda::
-    outer_product_scatter_add_with_weights_vjp(Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 3>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<int32_t, 1>, Tensor<int32_t, 1>) {
+    outer_product_scatter_add_with_weights_vjp(Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 3>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<scalar_t, 2>, Tensor<int32_t, 1>, Tensor<int32_t, 1>, void*) {
     throw std::runtime_error("MOPS was not compiled with CUDA support");
 }
 
 template <typename scalar_t>
-void mops::cuda::outer_product_scatter_add_with_weights_vjp_vjp(
-    Tensor<scalar_t, 3> /*grad_grad_output*/,
-    Tensor<scalar_t, 2> /*grad_A_2*/,
-    Tensor<scalar_t, 2> /*grad_B_2*/,
-    Tensor<scalar_t, 2> /*grad_W_2*/,
-    Tensor<scalar_t, 2> /*grad_grad_A*/,
-    Tensor<scalar_t, 2> /*grad_grad_B*/,
-    Tensor<scalar_t, 2> /*grad_grad_W*/,
-    Tensor<scalar_t, 3> /*grad_output*/,
-    Tensor<scalar_t, 2> /*A*/,
-    Tensor<scalar_t, 2> /*B*/,
-    Tensor<scalar_t, 2> /*W*/,
-    Tensor<int32_t, 1> /*indices_W*/,
-    Tensor<int32_t, 1> /*indices_output*/
-) {
+void mops::cuda::
+    outer_product_scatter_add_with_weights_vjp_vjp(Tensor<scalar_t, 3> /*grad_grad_output*/, Tensor<scalar_t, 2> /*grad_A_2*/, Tensor<scalar_t, 2> /*grad_B_2*/, Tensor<scalar_t, 2> /*grad_W_2*/, Tensor<scalar_t, 2> /*grad_grad_A*/, Tensor<scalar_t, 2> /*grad_grad_B*/, Tensor<scalar_t, 2> /*grad_grad_W*/, Tensor<scalar_t, 3> /*grad_output*/, Tensor<scalar_t, 2> /*A*/, Tensor<scalar_t, 2> /*B*/, Tensor<scalar_t, 2> /*W*/, Tensor<int32_t, 1> /*indices_W*/, Tensor<int32_t, 1> /*indices_output*/, void*) {
     throw std::runtime_error("MOPS was not compiled with CUDA support");
 }
 
@@ -114,7 +101,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights<float>(
     Tensor<float, 2> B,
     Tensor<float, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 template void mops::cuda::outer_product_scatter_add_with_weights<double>(
@@ -123,7 +111,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights<double>(
     Tensor<double, 2> B,
     Tensor<double, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 template void mops::cuda::outer_product_scatter_add_with_weights_vjp<float>(
@@ -135,7 +124,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights_vjp<float>(
     Tensor<float, 2> B,
     Tensor<float, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 template void mops::cuda::outer_product_scatter_add_with_weights_vjp<double>(
@@ -147,7 +137,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights_vjp<double>(
     Tensor<double, 2> B,
     Tensor<double, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 template void mops::cuda::outer_product_scatter_add_with_weights_vjp_vjp<float>(
@@ -163,7 +154,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights_vjp_vjp<float>(
     Tensor<float, 2> B,
     Tensor<float, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 template void mops::cuda::outer_product_scatter_add_with_weights_vjp_vjp<double>(
@@ -179,7 +171,8 @@ template void mops::cuda::outer_product_scatter_add_with_weights_vjp_vjp<double>
     Tensor<double, 2> B,
     Tensor<double, 2> W,
     Tensor<int32_t, 1> indices_W,
-    Tensor<int32_t, 1> indices_output
+    Tensor<int32_t, 1> indices_output,
+    void* cuda_stream
 );
 
 #endif
